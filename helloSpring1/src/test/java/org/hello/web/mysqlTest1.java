8ce06bf14208c056e757db2e053e4295469e8597
coke8711 <jun4404@gmail.com>
2018-05-09 01:48:55 +0900

    Spring 게시판만들기

---
+++ helloSpring1/src/test/java/org/hello/web/mysqlTest1.java
@@ -0,0 +1,26 @@
+package org.hello.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class mysqlTest1 {
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/test1";
+	private static final String USER = "root";
+	private static final String PW = "di5512dn";
+		
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			System.out.println("성공");
+			System.out.println(con);
+			
+		}catch(Exception e) {
+			System.out.println("에러발생");
+			e.printStackTrace();
+		}
+	}
+}
